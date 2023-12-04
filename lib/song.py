17ffd7d6566fa90eba2bbf34ac94cc1953525b43
TradingCyborg <my.github.kinyuad29@gmail.com>
2023-12-04 15:04:52 +0300

    Attributes

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,22 @@
 class Song:
-    pass
+    # Class attribute to keep track of the number of songs
+    count = 0
+
+    def __init__(self, title, artist):
+        self.title = title
+        self.artist = artist
+
+        # Call the class method to increment the count
+        Song.add_song_to_count()
+
+    @classmethod
+    def add_song_to_count(cls):
+        # Increment the class attribute count when a new song is created
+        cls.count += 1
+
+# Example usage:
+song1 = Song("Song 1", "Artist 1")
+song2 = Song("Song 2", "Artist 2")
+
+# Accessing the class attribute count
+print(f"Total number of songs: {Song.count}")
